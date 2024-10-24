649056a5703583966a1b21d488400a032d8c9bc3
Christopher <christopher.rydberg@iths.se>
2024-10-24 10:19:36 +0200

    Trying postgres

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,25 @@ const express = require('express'),
 
 const app = express();
 
+const dotenv = require('dotenv'),
+  { client } = require('pg');
+
+dotenv.config();
+
+const client = new client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
+
+app.get('/api', async (_request, response) => {
+  const { rows } = await client.query('SELECT * FROM cities WHERE name = $1', [
+    'Stockholm',
+  ]);
+
+  response.send(rows);
+});
+
 app.get('/api', (_request, response) => {
   response.send({ hello: 'World' });
 });
