3ca6e279fbde36f9ae2d26c71433e6264515c4cc
Christopher <christopher.rydberg@iths.se>
2024-10-25 12:48:49 +0200

    process.env.PORT

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express'),
   path = require('path');
 
 const app = express();
-const port = process.env.PGURI || 4000;
+const port = process.env.PORT || 4000;
 
 // app.get('/api', (_request, response) => {
 //   response.send({ hello: 'World' });
